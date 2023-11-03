7811401b7ab35aef460ad2b6db6a7d14202e0b39
Own0 <xinakoo@outlook.com>
2023-11-03 23:34:27 +0100

    V1

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -7,7 +7,7 @@ const logger = LoggerUtil.getLogger('ConfigManager')
 
 const sysRoot = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-const dataPath = path.join(sysRoot, '.azarlyamc')
+const dataPath = path.join(sysRoot, '.azarlya')
 
 const launcherDir = require('@electron/remote').app.getPath('userData')
 
